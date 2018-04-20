19a1d2aac8868a9792fb454aaca3299cca44251b
surenwork <surenwork@gmail.com>
2018-04-20 19:43:44 +0530

    added credentials to service

--- 2-setup_gcloud.sh
+++ 2-setup_gcloud.sh
@@ -3,7 +3,6 @@ echo "Reading deployment variables"
 . ~/.build_env
 
 echo "Setting up google cloud enviornment for deployment"
-cat ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 gcloud config set project $GCLOUD_PROJECT_NAME
 gcloud config set compute/region $GCLOUD_REGION
@@ -13,6 +12,7 @@ gcloud container clusters update $GCLOUD_KUBE_CLUSTER --no-enable-legacy-authori
 
 sed -i 's|$APP_PORT|'"$APP_PORT"'|' ~/.nginx.conf
 kubectl create configmap nginx-config --from-file=~/nginx.conf
+kubectl create secret generic service-account-creds --from-file=${HOME}/gcloud-service-key.json
 
 echo "Setup complete, Please see the details below"
 gcloud config list
